7355adb0a9f4ef24f60328acf1ccbb6a98aa14e6
Carlos Aponte <carlos@theflou.com>
2025-06-13 23:46:15 -0400

    fix: credential weather front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://py-ally-back-production.up.railway.app',
   apiWeather: '0d4cdfadad2540d5b74193937251306',
-  baseUrlWeather: 'http://api.weatherapi.com/v1',
+  baseUrlWeather: '/api/weather',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://py-ally-back-production.up.railway.app',
   apiWeather: '0d4cdfadad2540d5b74193937251306',
-  baseUrlWeather: 'http://api.weatherapi.com/v1',
+  baseUrlWeather: '/api/weather',
 };
